a933821ac9f8309ff95d5e0f9ad4496242d7f009
thedanieljk <thedanieljk@gmail.com>
2018-01-12 09:16:37 -0800

    mergesort 3 methods

--- Assignment1/Sorts.java
+++ Assignment1/Sorts.java
@@ -17,7 +17,32 @@ public class Sorts {
    }
 
    public static void mergeSort(int[] arr, int N) {
+      if (1<N) {
+         int mid = N/2;
 
+         sort(arr,
+      }
+      int n1 = N/2;
+      int n2 = N - n1;
+
+      int arr1[] = new int[n1];
+      int arr2[] = new int[n2];
+
+      for (int i=0;i<n1;++i)
+         arr1[i] = arr[i];
+      for (int j=n1;j<n2;++j) 
+         arr2[j] = arr[j];
+
+   }
+
+   private void sort(int arr[], int N) {
+
+   }
+
+   private void merge(int low, int middle, int high) {
+      for (int i =low;i<high;i++) {
+         helper
+      }
    }
 
    public static void quickSort(int[] arr, int N) {
