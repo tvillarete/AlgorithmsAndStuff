70c8e8b981db3b548205eda087339aa9fe40c80f
thedanieljk <thedanieljk@gmail.com>
2018-01-22 10:54:53 -0800

    Project 2 - Reads/stores/prints matrices from input file

--- Project2/MatrixWork.java
+++ Project2/MatrixWork.java
@@ -1,46 +1,62 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class MatrixWork {
    public static void main(String[] args) {
       System.out.println("Please enter the input-file's name");
       Scanner s = new Scanner(System.in);
       String filename = s.next();
-      System.out.println(filename);
-      File file = new File(filename);
-      int ctr = 0;
-      boolean matrix1 = true;
-      int matrix1[][];
-      int matrix2[][];
-      int rowct=0;
-      int colct=0;
+      Scanner file = new Scanner(System.in);
+      try {
+         file = new Scanner(new File(filename));
+      }
+      catch (Exception exc) {
+         System.out.println("Someone fucked up\n");
+      }
+      System.out.println("Reading from " + filename);
+      boolean isMatrix1 = true;
       while (file.hasNextInt()) {
          int row;
          int col;
-         if (matrix1) {
+         if (isMatrix1) {
             row = file.nextInt();
             col = file.nextInt();
-            matrix1= new int[row][col];
-            matrix1 = false;
+            int[][] matrix1 = new int[row][col];
+            matrix1 = fillMatrix(file,row,col);
+            printMatrix(matrix1);
+            isMatrix1 = false;
          }
          else { //matrix2;
             row = file.nextInt();
             col = file.nextInt();
-            matrix2 = new int[row][col];
+            int[][] matrix2 = new int[row][col];
+            matrix2 = fillMatrix(file,row,col);
+            printMatrix(matrix2);
 
          }
-
-         else if ((rowct == matrix1.length()) && (colct == matrix1[0].length())){
-         }
-         else {
-            matrix1[rowct][colct] = file.nextInt();
-         }
-         
-
       }
    }
 
-   private static void fillArray(int[][] arr,file) {
+   private static void printMatrix(int[][] arr) {
+      int row = arr.length;
+      int col = arr[0].length;
+      System.out.println("Printing a "+ row + " x " + col + " matrix");
+      for (int i=0;i<row;i++) {
+         for (int j=0;j<col;j++) {
+            System.out.print(arr[i][j]);
+         }
+         System.out.println();
+      }
+   }
 
+   private static int[][] fillMatrix(Scanner file,int row, int col) {
+      int[][] matrix = new int[row][col];
+      for (int i =0;i<row;i++) {
+         for (int j=0;j<col;j++) {
+            matrix[i][j] = file.nextInt();
+         }
+      }
+      return matrix;
    }
 /*
    public static int[][] matrixProduct(int[][] A, int[][]B) {
