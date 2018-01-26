6dfa73528d94843f4e382f3aa6bd585afc0806d7
tvillarete <tvillarete@gmail.com>
2018-01-26 09:53:33 -0800

    Add testing platform for part 2

---
+++ Project2/Part2/Application.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class Application {
+   public static void main(String[] args) {
+      System.out.println("Please enter the input-file's name");
+      Scanner s = new Scanner(System.in);
+      String filename = s.next();
+      Scanner file = new Scanner(System.in);
+      try {
+         file = new Scanner(new File(filename));
+         getMatrices(file);
+      }
+      catch (Exception exc) {
+         System.out.println("oOps\n");
+      }
+      //System.out.println("Reading from " + filename);
+   }
+
+   private static void getMatrices(Scanner file) {
+      boolean isMatrix1 = true;
+      int[][] matrix1 = new int[0][0];
+      int[][] matrix2 = new int[0][0];
+      while (file.hasNextInt()) {
+         int row;
+         int col;
+         if (isMatrix1) {
+            row = file.nextInt();
+            col = file.nextInt();
+            matrix1 = new int[row][col];
+            matrix1 = fillMatrix(file,row,col);
+            //printMatrix(matrix1);
+            isMatrix1 = false;
+         }
+         else { //matrix2
+            row = file.nextInt();
+            col = file.nextInt();
+            matrix2 = new int[row][col];
+            matrix2 = fillMatrix(file,row,col);
+            //printMatrix(matrix2);
+
+            try {
+               //System.out.println("resulting array should be " + matrix1.length + " x " + matrix2[0].length);
+               int[][] matrix3 = new int[matrix1.length][matrix2[0].length];
+               matrix3 = matrixProduct(matrix1,matrix2);
+               //System.out.println("Product matrix:");
+               printMatrix(matrix3);
+               printMatrixSize(matrix3);
+            }
+            catch (IllegalArgumentException e) {
+               System.out.println("Incompatible Matrices");
+            }
+         }
+      }
+      MatrixProduct.matrixProduct_DAC(matrix1, matrix2);
+   }
+
+   private static void printMatrix(int[][] arr) {
+      int row = arr.length;
+      int col = arr[0].length;
+      System.out.println();
+      for (int i=0;i<row;i++) {
+         for (int j=0;j<col;j++) {
+            System.out.print(arr[i][j] + " ");
+         }
+         System.out.println();
+      }
+   }
+
+   private static void printMatrixSize(int[][] arr) {
+      int row = arr.length;
+      int col = arr[0].length;
+
+      System.out.println();
+      System.out.println("C's size: " + row + "X" + col);
+   }
+
+   private static int[][] fillMatrix(Scanner file,int row, int col) {
+      int[][] matrix = new int[row][col];
+      for (int i =0;i<row;i++) {
+         for (int j=0;j<col;j++) {
+            matrix[i][j] = file.nextInt();
+         }
+      }
+      return matrix;
+   }
+
+   public static int[][] matrixProduct(int[][] A, int[][]B) {
+      int row1,row2,col1,col2;
+      row1 = A.length;
+      row2 = B.length;
+      col1 = A[0].length;
+      col2 = B[0].length;
+      if (col1 != row2) {
+         throw new IllegalArgumentException();
+      }
+      int[][] matrix3 = new int[row1][col2];
+      for (int i=0;i<row1;i++) {
+         for (int j=0;j<col2;j++) {
+            for (int k=0;k<col1;k++) {
+               matrix3[i][j] += A[i][k] * B[k][j];
+            }
+         }
+      }
+      return matrix3;
+   }
+}
+
--- Project2/Part2/MatrixProduct.java
+++ Project2/Part2/MatrixProduct.java
@@ -1,9 +1,34 @@
 public class MatrixProduct {
    public static int[][] matrixProduct_DAC(int[][] A, int[][] B) {
-
+      try {
+         checkValid(A, B);
+      } catch (IllegalArgumentException e) {
+         System.out.println("Requirements not satisfied");
+         System.exit(1);
+      }
+      return A;
    }
 
    public static int[][] matrixProduct_Strassen(int[][] A, int[][] B) {
+      try {
+         checkValid(A, B);
+      } catch (IllegalArgumentException e) {
+         System.out.println("Requirements not satisfied");
+         System.exit(1);
+      }
+      return B;
+   }
 
+   private static void checkValid(int[][] A, int[][] B)
+    throws IllegalArgumentException{
+      int rowA = A.length;
+      int colA = A[0].length;
+      int rowB = B.length;
+      int colB = B[0].length;
+
+      if (rowA != colA || rowB != colB || rowA != rowB ||
+          rowA%2 != 0) {
+         throw new IllegalArgumentException();
+      }
    }
 }
