a7d7329afd97280812807e28051d0961b8f0d346
tvillarete <tvillarete@gmail.com>
2018-02-02 09:48:39 -0800

    Add strassen's algorithm (NOT WORKING)

--- Project2/Part2/MatrixProduct.java
+++ Project2/Part2/MatrixProduct.java
@@ -46,12 +46,73 @@ public class MatrixProduct {
       return C;
    }
 
+   public static int[][] matrixProduct_Strassen(int[][] A, int[][] B) {
+
+   }
+
+   private static int[][] matrixProduct_Strassen(int[][] A, int[][] B,
+    int rowA, int colA, int rowB, int colB, int n) {
+      int[][] C = new int[n][n];   // Product matrix to return
+      if (n == 1) {
+         C[0][0] = A[rowA][colA] * B[rowB][colB];
+      } else {
+         int newSize = n/2;
+         sumMatrix(
+            C,
+            matrixProduct_DAC(A, B, rowA, colA, rowB, colB, newSize),
+            matrixProduct_DAC(A, B, rowA, colA + newSize, rowB + newSize, colB, newSize),
+            0, 0
+         );
+         sumMatrix(
+            C,
+            matrixProduct_DAC(A, B, rowA, colA, rowB, colB + newSize, newSize),
+            matrixProduct_DAC(A, B, rowA, colA + newSize, rowB + newSize, colB + newSize, newSize),
+            0, newSize
+         );
+         sumMatrix(
+            C,
+            matrixProduct_DAC(A, B, rowA + newSize, colA, rowB, colB, newSize),
+            matrixProduct_DAC(A, B, rowA + newSize, colA + newSize, rowB + newSize, colB, newSize),
+            newSize, 0
+         );
+         sumMatrix(
+            C,
+            matrixProduct_DAC(A, B, rowA + newSize, colA, rowB, colB + newSize, newSize),
+            matrixProduct_DAC(A, B, rowA + newSize, colA + newSize, rowB + newSize, colB + newSize, newSize),
+            newSize, newSize
+         );
+      }
+      return C;
+   }
+
    private static void sumMatrix(int[][] C, int[][] A, int[][] B, int rowC, int colC) {
       int n = A.length;
       for (int i=0; i<n; i++) {
          for (int j=0; j<n; j++) {
             C[i + rowC][j + colC] = A[i][j] + B[i][j];
          }
+      }
+   }
+
+   private static void diffMatrix(int[][] C, int[][] A, int[][] B, int rowC, int colC) {
+      int n = A.length;
+      for (int i=0; i<n; i++) {
+         for (int j=0; j<n; j++) {
+            C[i + rowC][j + colC] = A[i][j] - B[i][j];
+         }
+      }
+   }
+
+
+   private static void printMatrix(int[][] arr) {
+      int row = arr.length;
+      int col = arr[0].length;
+      System.out.println();
+      for (int i=0;i<row;i++) {
+         for (int j=0;j<col;j++) {
+            System.out.print(arr[i][j] + " ");
+         }
+         System.out.println();
       }
    }
 
