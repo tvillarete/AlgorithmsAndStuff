2a69903ff0daaa775c61750abb22b4c8629d66a1
tvillarete <tvillarete@gmail.com>
2018-03-02 19:01:20 -0800

    Fix Dynamic Programming Algorithm and Finish Main

--- Project4/ChangeMaker.java
+++ Project4/ChangeMaker.java
@@ -1,3 +1,11 @@
+/**
+ * Tanner Villarete (tvillare)
+ * Daniel Kirkpatrick (djkirkpa)
+ * CSC 349
+ * 3/2/18
+ * Project 4
+ */
+
 import java.util.Scanner;
 
 public class ChangeMaker {
@@ -6,23 +14,29 @@ public class ChangeMaker {
       int[] coins;
       int n;
       Scanner s = new Scanner(System.in);
-      System.out.println("Enter the number of coins and the denomination: ");
+      System.out.println("Enter the number of coin-denominations and the set of coin values:");
       numCoins = s.nextInt();
       coins = new int[numCoins];
       for (int i=0; i<numCoins; i++) {
          coins[i] = s.nextInt();
       }
-      System.out.println("Enter n: ");
+      System.out.println("\nEnter a positive amount to be changed (enter 0 to quit): ");
       n = s.nextInt();
-      if (n == 0) {
-         System.exit(0);
+      while (n != 0) {
+         if (n < 0) {
+            System.out.println("You entered a negative number.");
+         } else {
+            int DPres[] = change_DP(n, coins);
+            int GRres[] = change_greedy(n, coins);
+            System.out.println("\nDP algorithm results");
+            printRes(n,DPres,coins);
+            System.out.println("\nGreedy algorithm results");
+            printRes(n,GRres,coins);
+            System.out.println("\nEnter a positive amount to be changed (enter 0 to quit): ");
+            n = s.nextInt();
+         }
       }
-      int DPres[] = change_DP(n, coins);
-      int GRres[] = change_greedy(n, coins);
-      System.out.println("\nDP algorithm results");
-      printRes(n,DPres,coins);
-      System.out.println("\nGreedy algorithm results");
-      printRes(n,GRres,coins);
+      System.out.println("Thanks for playing. Good Bye.");
    }
 
    private static void printRes(int n,int[] res,int[] d) {
@@ -42,58 +56,32 @@ public class ChangeMaker {
    }
 
    public static int[] change_DP(int n, int[] d) {
-      int[] coins = new int[d.length];
-      int centsLeft = n;
-      /*
-      for (int i=0; i<d.length; i++) {
-         int count = 0, numCoins = 0, total = 0;
-         int prevNumCoins = numCoins;
-
-         while (count <= centsLeft) {
-            prevNumCoins = numCoins;
-            count += d[i];
-            numCoins++;
-         }
-         coins[i] = prevNumCoins;
-         centsLeft -= (coins[i] * d[i]);
-      }*/
-
-      int[][] sol = new int[n+1][];
-      int[] best = new int[n+1];
-
-      sol[0] = new int[n];
-      best[0] = 0;
-
-      for (int i = 1; i <= n; i++) {
-         solveN(i,d,sol,best);
-      }
-      return best;
-   }
-
-   private static void solveN(int n, int[] d, int[][] sol, int[] best) {
-      int numCoins = d.length;
-
-      for (int i=0; i< numCoins;i++) {
-         if (d[i] == n) {
-            sol[n] = new int[numCoins];
-            sol[n][i] = 1;
-            best[i] = 1;
-            return;
-         }
-         else {
-            int least = Integer.MAX_VALUE;
-            for (int j = 1; j< n; j++) {
-               int coinCount = best[j] + best[n-1];
-               if (coinCount < least) {
-                  least = coinCount;
-                  best[n] = coinCount;
-                  //sol[n] 
-                  sol[n] = sol[i] + sol[n-1];
-               }
+      int[] coins = new int[n+1], table = new int[n+1], solution = new int[d.length];
+      // Base case
+      coins[0] = 0;
+      // Iterate through possibilities
+      for (int i=1; i<n+1; i++) {
+         int j = 0;
+         while (d[j] > i)
+            j++;
+         int min = coins[i - d[j]];
+         table[i] = j;
+         for (int l=j+1; l<d.length; l++) {
+            if (min > coins[i-d[l]]) {
+               min = coins[i- d[l]];
+               table[i] = l;
             }
          }
-
+         coins[i] = min + 1;
       }
+
+      int count = coins[n], temp = 0, tableSpot = n;
+      while (count-- > 0) {
+         temp=table[tableSpot];
+         solution[temp]++;
+         tableSpot = tableSpot-d[temp];
+      }
+      return solution;
    }
 
    public static int[] change_greedy(int n, int[] d) {
